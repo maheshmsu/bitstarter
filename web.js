b66531f2d5947acd9a8cd72f462031a2be00252d
mahesh muthyam <work.mahesh@live.com>
2013-07-11 15:35:59 +0000

    added require fs

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  	def buffer = fs.readFileSync("index.html");
+  	var buffer = fs.readFileSync("index.html");
 	response.send(buffer.toString());
 });
 
